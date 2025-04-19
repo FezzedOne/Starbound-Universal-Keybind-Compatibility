d6960f0b700735b0e05bbe63a1443c8e7a123910
FezzedOne <RenoFezzedOne@yandex.com>
2025-04-19 15:49:16 -0400

    [v1.0.1r2] Removed debug prints.

--- scripts/fezzedone-keybind-patch.lua
+++ scripts/fezzedone-keybind-patch.lua
@@ -1,10 +1,8 @@
 local potentialOldKeybindScripts = assets.scan("keybinds.lua")
 local keybindHeaderPattern = "%-%-%[%[[%s\n]*Keybinds Library"
 for _, scriptPath in ipairs(potentialOldKeybindScripts) do
-    sb.logInfo("Potential keybind scripts: %s", sb.printJson(potentialOldKeybindScripts))
     local scriptFile = assets.bytes(scriptPath)
     if scriptFile:find(keybindHeaderPattern, nil, false) then
-        sb.logInfo("Found Silverfeelin keybind header at '%s'.", scriptPath)
         -- This is a Silverfeelin keybind script. Replace any `if input then` check.
         local replacedOld = false
         scriptFile = scriptFile:gsub("(if input then)", function(s)
