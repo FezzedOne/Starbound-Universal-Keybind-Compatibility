fe71682d7d4dd2c4bc6a71be002a5c1a2b311d72
FezzedOne <RenoFezzedOne@yandex.com>
2025-04-19 15:10:28 -0400

    [v1.0.1] Fixed patch script not detecting some keybind scripts; fixed typo in callback name.

--- scripts/fezzedone-keybind-patch.lua
+++ scripts/fezzedone-keybind-patch.lua
@@ -1,8 +1,10 @@
 local potentialOldKeybindScripts = assets.scan("keybinds.lua")
-local keybindHeader = "--[[\nKeybinds Library"
+local keybindHeaderPattern = "%-%-%[%[[%s\n]*Keybinds Library"
 for _, scriptPath in ipairs(potentialOldKeybindScripts) do
+    sb.logInfo("Potential keybind scripts: %s", sb.printJson(potentialOldKeybindScripts))
     local scriptFile = assets.bytes(scriptPath)
-    if scriptFile:find(keybindHeader, nil, true) then
+    if scriptFile:find(keybindHeaderPattern, nil, false) then
+        sb.logInfo("Found Silverfeelin keybind header at '%s'.", scriptPath)
         -- This is a Silverfeelin keybind script. Replace any `if input then` check.
         local replacedOld = false
         scriptFile = scriptFile:gsub("(if input then)", function(s)
@@ -10,7 +12,7 @@ for _, scriptPath in ipairs(potentialOldKeybindScripts) do
             return [=[if type(input) == "function" then]=]
         end)
         if replacedOld then
-            assets.remove(scriptPath)
+            assets.erase(scriptPath)
             assets.add(scriptPath, scriptFile)
             if assets.origin and assets.sourceMetadata then -- Hopefully oSB gets a metadata callback soon.
                 local originPath = assets.origin(scriptPath)
