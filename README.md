5fa19601f5dfd2dfce9d1e90aaa06758e3ccbb8f
FezzedOne <RenoFezzedOne@yandex.com>
2025-04-19 14:41:07 -0400

    [v1.0] Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+Fixes a compatibility issue between old versions of Silverfeelin's Keybinds script and xStarbound/OpenStarbound where the script silently bugs out upon detecting these mods' _input_ table, preventing keybinds from working.
+
+# Compatibility
+
+This mod fixes any keybind script _anywhere_ that looks like an old version of Silverfeelin's script. There may be potential compatibility issues if a modified Silverfeelin keybind script somehow actually runs a check for xStarbound's or OpenStarbound's _input_ table, but this is extremely unlikely because xStarbound and OpenStarbound provide a **Mod Binds** pane and custom bind support in the engine.
+
+**Mod Pack Permissions:** Anyone can include this mod in compilations without the author's consent.
+
+**Mod Assets Permissions:** Anyone can alter/redistribute the mod's assets without the author's consent.
---
+++ scripts/fezzedone-keybind-patch.lua
@@ -0,0 +1,37 @@
+local potentialOldKeybindScripts = assets.scan("keybinds.lua")
+local keybindHeader = "--[[\nKeybinds Library"
+for _, scriptPath in ipairs(potentialOldKeybindScripts) do
+    local scriptFile = assets.bytes(scriptPath)
+    if scriptFile:find(keybindHeader, nil, true) then
+        -- This is a Silverfeelin keybind script. Replace any `if input then` check.
+        local replacedOld = false
+        scriptFile = scriptFile:gsub("(if input then)", function(s)
+            replacedOld = true
+            return [=[if type(input) == "function" then]=]
+        end)
+        if replacedOld then
+            assets.remove(scriptPath)
+            assets.add(scriptPath, scriptFile)
+            if assets.origin and assets.sourceMetadata then -- Hopefully oSB gets a metadata callback soon.
+                local originPath = assets.origin(scriptPath)
+                local sourceMetadata = assets.sourceMetadata(originPath)
+                local sourceName = sourceMetadata.friendlyName or sourceMetadata.name or originPath
+                sourceName = type(sourceName) == "string" and sourceName or sb.printJson(sourceName)
+                sb.logInfo(
+                    "[Keybind Patch] Patched old Silverfeelin keybind script at '%s', owned by '%s'",
+                    scriptPath,
+                    sourceName
+                )
+            elseif assets.origin then
+                local originPath = assets.origin(scriptPath)
+                sb.logInfo(
+                    "[Keybind Patch] Patched old Silverfeelin keybind script at '%s', owned by asset source at '%s'",
+                    scriptPath,
+                    originPath
+                )
+            else
+                sb.logInfo("[Keybind Patch] Patched old Silverfeelin keybind script at '%s'", scriptPath)
+            end
+        end
+    end
+end
